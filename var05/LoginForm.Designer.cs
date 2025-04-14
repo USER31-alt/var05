bb8b1f556a268627d60ef9da89329ae0fe11e589
User31-alt <vika.z.0505@mail.ru>
2025-04-14 13:26:00 +0300

    CLOSE #4: CAPTHCA

--- var05/LoginForm.Designer.cs
+++ var05/LoginForm.Designer.cs
@@ -33,14 +33,14 @@ namespace var05
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.passwordButton = new System.Windows.Forms.Button();
+            this.userHame = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.CaptchaTextBox = new System.Windows.Forms.TextBox();
+            this.CaptchaImage = new System.Windows.Forms.PictureBox();
+            this.PasswordB = new System.Windows.Forms.Button();
+            this.PasswordB33 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CaptchaImage)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -83,68 +83,71 @@ namespace var05
             this.label4.TabIndex = 3;
             this.label4.Text = "Пароль";
             // 
-            // button1
+            // passwordButton
             // 
-            this.button1.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.button1.Font = new System.Drawing.Font("Autoproject  GOST Type A", 15.75F);
-            this.button1.Location = new System.Drawing.Point(138, 327);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 36);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Войти";
-            this.button1.UseVisualStyleBackColor = false;
+            this.passwordButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.passwordButton.Font = new System.Drawing.Font("Autoproject  GOST Type A", 15.75F);
+            this.passwordButton.Location = new System.Drawing.Point(138, 327);
+            this.passwordButton.Name = "passwordButton";
+            this.passwordButton.Size = new System.Drawing.Size(90, 36);
+            this.passwordButton.TabIndex = 4;
+            this.passwordButton.Text = "Войти";
+            this.passwordButton.UseVisualStyleBackColor = false;
+            this.passwordButton.Click += new System.EventHandler(this.LoginButton_Click);
             // 
-            // textBox1
+            // userHame
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(116, 127);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(152, 26);
-            this.textBox1.TabIndex = 5;
+            this.userHame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.userHame.Location = new System.Drawing.Point(116, 127);
+            this.userHame.Name = "userHame";
+            this.userHame.Size = new System.Drawing.Size(152, 26);
+            this.userHame.TabIndex = 5;
             // 
-            // textBox2
+            // Password
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox2.Location = new System.Drawing.Point(116, 166);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(152, 26);
-            this.textBox2.TabIndex = 6;
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Password.Location = new System.Drawing.Point(116, 166);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(152, 26);
+            this.Password.TabIndex = 6;
             // 
-            // textBox3
+            // CaptchaTextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(128, 283);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 7;
+            this.CaptchaTextBox.Location = new System.Drawing.Point(128, 283);
+            this.CaptchaTextBox.Name = "CaptchaTextBox";
+            this.CaptchaTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CaptchaTextBox.TabIndex = 7;
             // 
-            // pictureBox1
+            // CaptchaImage
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(116, 215);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(152, 46);
-            this.pictureBox1.TabIndex = 8;
-            this.pictureBox1.TabStop = false;
+            this.CaptchaImage.Location = new System.Drawing.Point(116, 215);
+            this.CaptchaImage.Name = "CaptchaImage";
+            this.CaptchaImage.Size = new System.Drawing.Size(152, 46);
+            this.CaptchaImage.TabIndex = 8;
+            this.CaptchaImage.TabStop = false;
             // 
-            // button2
+            // PasswordB
             // 
-            this.button2.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.button2.Font = new System.Drawing.Font("Autoproject  GOST Type A", 15.75F);
-            this.button2.Location = new System.Drawing.Point(274, 166);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(48, 26);
-            this.button2.TabIndex = 9;
-            this.button2.UseVisualStyleBackColor = false;
+            this.PasswordB.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.PasswordB.Font = new System.Drawing.Font("Autoproject  GOST Type A", 15.75F);
+            this.PasswordB.Location = new System.Drawing.Point(220, 166);
+            this.PasswordB.Name = "PasswordB";
+            this.PasswordB.Size = new System.Drawing.Size(48, 26);
+            this.PasswordB.TabIndex = 9;
+            this.PasswordB.UseVisualStyleBackColor = false;
+            this.PasswordB.Click += new System.EventHandler(this.ShowPasswordButton_Click);
             // 
-            // button3
+            // PasswordB33
             // 
-            this.button3.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.button3.Font = new System.Drawing.Font("Autoproject  GOST Type A", 15.75F);
-            this.button3.Location = new System.Drawing.Point(234, 283);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(50, 22);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = false;
+            this.PasswordB33.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.PasswordB33.Font = new System.Drawing.Font("Autoproject  GOST Type A", 15.75F);
+            this.PasswordB33.Location = new System.Drawing.Point(234, 283);
+            this.PasswordB33.Name = "PasswordB33";
+            this.PasswordB33.Size = new System.Drawing.Size(50, 22);
+            this.PasswordB33.TabIndex = 10;
+            this.PasswordB33.Text = "button3";
+            this.PasswordB33.UseVisualStyleBackColor = false;
+            this.PasswordB33.Click += new System.EventHandler(this.RefreshCaptchaButton_Click);
             // 
             // LoginForm
             // 
@@ -152,20 +155,20 @@ namespace var05
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.OldLace;
             this.ClientSize = new System.Drawing.Size(378, 384);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PasswordB33);
+            this.Controls.Add(this.PasswordB);
+            this.Controls.Add(this.CaptchaImage);
+            this.Controls.Add(this.CaptchaTextBox);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.userHame);
+            this.Controls.Add(this.passwordButton);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "LoginForm";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CaptchaImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,13 +180,13 @@ namespace var05
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button passwordButton;
+        private System.Windows.Forms.TextBox userHame;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.TextBox CaptchaTextBox;
+        private System.Windows.Forms.PictureBox CaptchaImage;
+        private System.Windows.Forms.Button PasswordB;
+        private System.Windows.Forms.Button PasswordB33;
     }
 }
 
--- var05/LoginForm.cs
+++ var05/LoginForm.cs
@@ -7,14 +7,121 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using var05;
 
 namespace var05
 {
     public partial class LoginForm : Form
     {
+        private const string DefaultUsername = "user";
+        private const string DefaultPassword = "user";
+        private string _captchaText;
         public LoginForm()
         {
             InitializeComponent();
+            RefreshCaptcha();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            string username = userHame.Text.Trim();
+            string password = Password.Text.Trim();
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Пожалуйста, введите логин и пароль.", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (username == DefaultUsername && password == DefaultPassword)
+            {
+                OpenMainForm();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                userHame.Clear();
+                Password.Clear();
+                Password.UseSystemPasswordChar = true;
+                passwordButton.Text = "👁";
+                RefreshCaptcha();
+            }
+        }
+
+        private void OpenMainForm()
+        {
+         //   MainForm mainForm = new MainForm();
+          //  mainForm.Show(); 
+        }
+
+        private void RefreshCaptchaButton_Click(object sender, EventArgs e)
+        {
+            RefreshCaptcha();
+        }
+
+        private void RefreshCaptcha()
+        {
+            _captchaText = GenerateCaptchaText(4);
+            CaptchaImage.Image = GenerateCaptchaImage(_captchaText);
+            CaptchaTextBox.Text = "";
+        }
+
+        private string GenerateCaptchaText(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            string result = "";
+            for (int i = 0; i < length; i++)
+            {
+                result += chars[random.Next(chars.Length)];
+            }
+            return result;
+        }
+
+        private Bitmap GenerateCaptchaImage(string captchaText)
+        {
+            Random rand = new Random();
+            int width = 150;
+            int height = 50;
+
+            Bitmap bmp = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(bmp);
+
+            g.Clear(Color.White);
+
+            Font font = new Font("Arial", 24);
+            float x = 10;
+            float yOffset = 0;
+
+            for (int i = 0; i < captchaText.Length; i++)
+            {
+                yOffset = rand.Next(-5, 6);
+                float rotationAngle = rand.Next(-10, 11);
+
+                Matrix matrix = new Matrix();
+                matrix.RotateAt(rotationAngle, new PointF(x, height / 2 + yOffset));
+                g.Transform = matrix;
+
+                g.DrawString(captchaText[i].ToString(), font, Brushes.Black, x, (height - font.GetHeight()) / 2 + yOffset);
+
+                g.ResetTransform();
+
+                x += g.MeasureString(captchaText[i].ToString(), font).Width + 5;
+
+                if (rand.Next(0, 2) == 0)
+                {
+                    g.DrawLine(Pens.Red, x - g.MeasureString(captchaText[i].ToString(), font).Width - 5, height / 2, x - 5, height / 2);
+                }
+            }
+
+            g.Dispose();
+            return bmp;
+        }
+        private void ShowPasswordButton_Click(object sender, EventArgs e)
+        {
+            Password.UseSystemPasswordChar = !Password.UseSystemPasswordChar;
+            PasswordB.Text = Password.UseSystemPasswordChar ? "👁" : "👁‍🗨";
         }
     }
 }
